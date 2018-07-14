f010e40cdb95b811e74e6713b17359eb1f0000e1
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-07-14 18:59:19 +0800

    Initial commit

---
+++ include/vector.hpp
@@ -0,0 +1,7 @@
+#include <cmath>
+
+struct vector {
+	float components[3] {};
+
+	vector() = default;
+};
---
+++ source/first-cmake.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "vector.hpp"
+
+int main() {
+	vector test {};
+
+	std::system("pause");
+	return EXIT_SUCCESS;
+}
